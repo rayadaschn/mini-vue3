b105676dd7b8b9d8eebac3e35e9dd6bb07a5c253
rayadaschn <115447518+rayadaschn@users.noreply.github.com>
2023-12-05 00:01:58 +0800

    feat: 增加 computed 响应性

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -8,6 +8,14 @@ export const isArray = Array.isArray
  * @description: 判断是否为数组
  * @return {boolean}
  */
-export const isObject = (val: unknown) => {
+export const isObject = (val: unknown): boolean => {
   return val !== null && typeof val === 'object'
+}
+
+/**
+ * @description: 判断是否为函数
+ * @return {boolean}
+ */
+export const isFunction = (val: unknown): boolean => {
+  return Object.prototype.toString.call(val) === '[object Function]'
 }
